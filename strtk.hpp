db85752b6b6b82dffc18072f15c60f67b094073f
Arash Partow <partow@gmail.com>
2018-04-15 11:55:52 +1000

    C++ String Toolkit Library (StrTk)  http://www.partow.net/programming/strtk/index.html

--- strtk.hpp
+++ strtk.hpp
@@ -12173,52 +12173,35 @@ namespace strtk
    }
    #endif // strtk_enable_random
 
-   template <typename Iterator>
-   inline bool next_combination(const Iterator first, Iterator k, const Iterator last)
+   template <class Iterator>
+   bool next_combination(Iterator const first, Iterator const k, Iterator const last)
    {
-      /* Credits: Thomas Draper */
-      if ((first == last) || (first == k) || (last == k))
+      if (
+           (first == last) ||
+           (first == k   ) ||
+           (last  == k   ) ||
+           (last  == std::next(first))
+         )
          return false;
 
-      Iterator itr1 = first;
-      Iterator itr2 = last;
-      ++itr1;
-
-      if (last == itr1)
-         return false;
-
-      itr1 = last;
-      --itr1;
-      itr1 = k;
-      --itr2;
+      Iterator itr1 = k;
+      Iterator itr2 = std::prev(last);
 
       while (first != itr1)
       {
-         if (*--itr1 < (*itr2))
+         if (*--itr1 < *itr2)
          {
             Iterator j = k;
-
-            while (!((*itr1) < (*j))) ++j;
-
-            std::iter_swap(itr1,j);
-            ++itr1;
-            ++j;
-            itr2 = k;
-            std::rotate(itr1,j,last);
-
-            while (last != j)
-            {
-               ++j;
-               ++itr2;
-            }
-
-            std::rotate(k,itr2,last);
+            while (!(*itr1 < *j)) ++j;
+            std::iter_swap(itr1++, j++);
+            std::rotate(itr1, j, last);
+            for (itr2 = k; j != last; ++j, ++itr2);
+            std::rotate(k, itr2, last);
             return true;
          }
       }
 
-      std::rotate(first,k,last);
-
+      std::rotate(first, k, last);
       return false;
    }
 
