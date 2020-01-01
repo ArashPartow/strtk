d2b446bf1f7854e8b08f5295ec6f6852cae066a2
Arash Partow <partow@gmail.com>
2020-01-01 00:00:00 +0000

    C++ String Toolkit Library (StrTk)  http://www.partow.net/programming/strtk/index.html

--- strtk_search_trie_example.cpp
+++ strtk_search_trie_example.cpp
@@ -3,7 +3,7 @@
  *                     String Toolkit Library                    *
  *                                                               *
  * Search Trie Example                                           *
- * Author: Arash Partow (2002-2019)                              *
+ * Author: Arash Partow (2002-2020)                              *
  * URL: http://www.partow.net/programming/strtk/index.html       *
  *                                                               *
  * Copyright notice:                                             *
